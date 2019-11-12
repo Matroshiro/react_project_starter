2a16b37ded8323b95ac4c805ac2899bc7b1c2a10
Tristan Goubeau <tristan.goubeau@epitech.eu>
2019-11-12 12:03:55 +0100

    add scss handling

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles/style.scss";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My homemade trello</h1>
+            </div>);
+    }
+}
+export default App;
